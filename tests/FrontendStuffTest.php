a6ff540f87dd64dc2cef854a3c9406f6f88d6c82
Marcus <marcusjian@awoo.com.tw>
2020-11-24 21:41:21 +0800

    reinstall phpunit selenium

--- tests/FrontendStuffTest.php
+++ tests/FrontendStuffTest.php
@@ -1,16 +1,12 @@
 <?php
 
-//require 'vendor/autoload.php';
-
-use PHPUnit\Extensions\Selenium2TestCase;
-
-class FrontendStuffTest extends Selenium2TestCase
+class FrontendStuffTest extends PHPUnit_Extensions_Selenium2TestCase
 {
     public function setUp(): void
     {
         $this->setBrowserUrl('http://localhost:8000/');
         $this->setBrowser('chrome');
-        $this->setDesiredCapabilities(['chormeOptions' => ['w3c' => false]]);
+        $this->setDesiredCapabilities(['chromeOptions' => ['w3c' => false]]);
     }
 
     public function testCanSeeCorrectStringsOnMainPage()
